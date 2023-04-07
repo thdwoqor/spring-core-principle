7e7b026fe0ef0fcb5e5cc5c00afb82a31a6d5209
thdwoqor <celebarte01@gmail.com>
2023-04-07 21:24:29 +0900

    주문, 할인 도메인 설계

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private final int discountFixAmount = 1000;
+
+    @Override
+    public int discount(final Member member, final int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }
+        return 0;
+    }
+}
