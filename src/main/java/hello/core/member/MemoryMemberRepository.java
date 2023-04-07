e019d692e7853027c5c3bea7d6d3e53997769eb8
thdwoqor <celebarte01@gmail.com>
2023-04-07 21:20:00 +0900

    회원 도메인 설계

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static final Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(final Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(final Long memberId) {
+        return store.get(memberId);
+    }
+}
